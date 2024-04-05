85f7481a3528d08b7ac317f2436d98e0c2a65545
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-04-05 15:29:22 +0700

    first commit

---
+++ REDME.md
@@ -0,0 +1,5 @@
+20220140177
+Muhammad Adifa Firmansyah
+
+![Hasil website 1](picture/Screenshot%202024-04-05%20at%2014.55.37.png)
+![Hasil website 2](picture/Screenshot%202024-04-05%20at%2014.55.44.png)
---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("video").addEventListener("click", function() {
+    alert("ini video Serana");
+});
+
+document.getElementById("audio").addEventListener("click", function() {
+    alert("ini Audio Serana");
+});
+
+document.getElementById("Gambar").addEventListener("click", function() {
+    alert("ini logo Serana");
+});
